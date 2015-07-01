2d76bb1874c55b4f347c93e0bd4b7b4a079cb842
lentan <lentan1029@gmail.com>
2015-07-01 18:51:02 +0800

    changed pkg ver

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "lentan:factory",
   summary: "Factories for Meteor",
-  version: "0.3.11",
+  version: "0.3.12",
   git: "https://github.com/lentan1029/meteor-factory.git"
 });
 
